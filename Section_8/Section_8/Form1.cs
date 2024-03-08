431e5043d9bc4d3dfc10bf43a809e02d91db666f
Arioo Barzan <arioobarzan@hotmail.com>
2024-03-08 13:52:08 +0330

    Create Form1.cs

---
+++ Section_8/Section_8/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Section_8
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name;
+            name = textBox1.Text;
+            string msg = "Hello " + name;
+            MessageBox.Show(msg);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox2.Text);
+            MessageBox.Show((i * 2).ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            try
+            {
+                x = int.Parse(textBox3.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBox3.Text = "";
+                return;
+            }
+            int f1 = 0, f2 = 1, f3 = 1;
+            if (x == 1)
+                MessageBox.Show("0");
+            else if (x == 2)
+                MessageBox.Show("1");
+            else if (x > 2)
+            {
+                for (int i = 3; i <= x; i++)
+                {
+                    f3 = f2 + f1;
+                    f1 = f2;
+                    f2 = f3;
+                }
+                MessageBox.Show(f3.ToString());
+            }
+            else
+                MessageBox.Show("Wrong input");
+        }
+    }
+}
